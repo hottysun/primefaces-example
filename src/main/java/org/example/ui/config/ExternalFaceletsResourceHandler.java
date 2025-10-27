23767e7c78ed70f98c66b0173c3955119ecc7474
sunny <justonlinepractise@gmail.com>
2025-10-28 01:02:51 +0530

    SHREE GANESH PRASANNA: 2. externalized xhtml (refresh 1 sec) working

---
+++ src/main/java/org/example/ui/config/ExternalFaceletsResourceHandler.java
@@ -0,0 +1,107 @@
+package org.example.ui.config;
+
+import jakarta.faces.application.Resource;
+import jakarta.faces.application.ResourceHandler;
+import jakarta.faces.context.FacesContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Custom ResourceHandler that serves Facelets from an external folder.
+ */
+public class ExternalFaceletsResourceHandler extends ResourceHandler {
+
+  private final ResourceHandler wrapped;
+  private final String externalPath;
+
+  public ExternalFaceletsResourceHandler(ResourceHandler wrapped) {
+    this.wrapped = wrapped;
+    this.externalPath = System.getProperty("facelets.external.path",
+                                           "C:\\Users\\sunny\\IdeaProjects\\primefaces-example1"
+                                               + "\\pages");
+  }
+
+  @Override
+  public Resource createResource(String resourceName) {
+    File file = new File(externalPath, resourceName);
+    if (file.exists() && file.isFile()) {
+      try {
+        URL url = file.toURI().toURL();
+        return new Resource() {
+          @Override
+          public String getRequestPath() {
+            return file.getName();
+          }
+
+          @Override
+          public URL getURL() {
+            return url;
+          }
+
+          @Override
+          public boolean userAgentNeedsUpdate(FacesContext facesContext) {
+            return false;
+          }
+
+          @Override
+          public InputStream getInputStream() {
+            try {
+              return new FileInputStream(file);
+            } catch (Exception e) {
+              throw new RuntimeException(e);
+            }
+          }
+
+          @Override
+          public Map<String, String> getResponseHeaders() {
+            return Collections.emptyMap();
+          }
+
+          @Override
+          public String getContentType() {
+            return "application/xhtml+xml";
+          }
+        };
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    // delegate to the default handler
+    return wrapped.createResource(resourceName);
+  }
+
+  @Override
+  public Resource createResource(String s, String s1) {
+    return null;
+  }
+
+  @Override
+  public Resource createResource(String s, String s1, String s2) {
+    return null;
+  }
+
+  @Override
+  public boolean isResourceRequest(FacesContext context) {
+    return wrapped.isResourceRequest(context);
+  }
+
+  @Override
+  public void handleResourceRequest(FacesContext context) throws java.io.IOException {
+    wrapped.handleResourceRequest(context);
+  }
+
+  @Override
+  public boolean libraryExists(String libraryName) {
+    return wrapped.libraryExists(libraryName);
+  }
+
+  @Override
+  public String getRendererTypeForResourceName(String resourceName) {
+    return wrapped.getRendererTypeForResourceName(resourceName);
+  }
+
+}
---
+++ src/main/java/org/example/ui/config/FacesApplicationConfig.java
@@ -0,0 +1,10 @@
+package org.example.ui.config;
+
+import jakarta.faces.annotation.FacesConfig;
+import jakarta.faces.annotation.FacesConfig.Version;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@FacesConfig(version = Version.JSF_2_3)
+public class FacesApplicationConfig {
+}
